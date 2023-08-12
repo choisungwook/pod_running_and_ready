fc02459b3a090ddf7207f11b57ec4897171671ab
Choisungwook <kgg1959@naver.com>
2023-08-12 18:05:04 +0900

    python app sleep 지속시간 수정

--- python_app/entrypoint.sh
+++ python_app/entrypoint.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 set -e
 
-# 장애 재현을 위해 sleep
-echo "sleep ..."
-sleep 20
+# 파이썬 애플리케이션 부팅시간 설정
+echo "[INFO] booting... wait a mintues..."
+# sleep 2분
+sleep 120
 
-echo "run python applicatoin"
+echo "[INFO] run python applicatoin"
 uvicorn main:app --host 0.0.0.0 --port 80
